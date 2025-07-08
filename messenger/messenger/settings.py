7518d1dc353ac11650ba478fee468d095c661178
shiverin <shiverinz@gmail.com>
2025-07-09 02:31:01 +0800

    updated project

--- messenger/messenger/settings.py
+++ messenger/messenger/settings.py
@@ -84,8 +84,12 @@ ASGI_APPLICATION = 'messenger.asgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.kiyixitqtefqrxajhmza',  # note the username with suffix
+        'PASSWORD': 'shiverin123&Z',
+        'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
+        'PORT': '5432',  # or '6543' for transaction pooler
     }
 }
 
